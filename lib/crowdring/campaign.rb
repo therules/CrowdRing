75747f92154028a5a0b93aac900fd9c4cc5b1e54
Willa <wwang@thoughtworks.com>
2012-09-19 07:48:02 -0400

    Added icon to title bar

--- lib/crowdring/campaign.rb
+++ lib/crowdring/campaign.rb
@@ -6,7 +6,7 @@ module Crowdring
     property :title,        String
 
     has n, :supporters, constraint: :destroy
-
+    
     def pretty_phone_number
       number = Phoner::Phone.parse phone_number
       number.format("+%c (%a) %n") + " [" + Phoner::Country.find_by_country_code(number.country_code).char_3_code + "]"
