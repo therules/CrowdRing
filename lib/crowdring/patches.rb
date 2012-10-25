88829b1471a71689581f98862cb3e3df58bc8e96
Nathan Herzing <nherzing@gmail.com>
2012-10-25 15:44:47 -0400

    Adding in sending of email to new user.

--- lib/crowdring/patches.rb
+++ lib/crowdring/patches.rb
@@ -1,3 +1,5 @@
+require 'net/smtp'
+
 module Sinatra
   module Helpers
     def login_required
@@ -12,13 +14,36 @@ module Sinatra
 
       false
     end
+
+   def send_password_email(to, created_by, password)
+      message = <<MESSAGE_END
+To: <#{to}>
+Subject: Welcome to Crowdring!
+
+Hello #{to},
+
+#{created_by} created a Crowdring account for you. Login at campaign dot crowdring dot org
+Your username is #{to}
+Your password is #{password}
+
+Happy campaigning!
+The Crowdring Team
+MESSAGE_END
+
+      Net::SMTP.start(ENV["SMTP_HOST"], ENV["SMTP_PORT"],
+          ENV["SMTP_DOMAIN"], ENV["SMTP_USER"], ENV["SMTP_PASSWORD"], :login) do |smtp|
+        smtp.send_message message, ENV["SMTP_USER"], to
+      end
+    end 
   end
 end
+
 
 module Sinatra
   module SinatraAuthentication
     class << self
       alias_method :orig_registered, :registered
+
 
       def registered(app)
         orig_registered(app)
@@ -33,9 +58,9 @@ module Sinatra
             redirect '/newuser?' + hash_to_query_string(params)
           else
             password = PasswordGenerator.generate
-            p password
             @user = User.set(email: params[:email], password: password, password_confirmation: password)
             if @user.valid && @user.id
+              send_password_email(params[:email], current_user.email, password)
               flash[:notice] = "Account created."
               redirect '/users'
             else
