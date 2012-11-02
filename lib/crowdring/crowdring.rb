8f14ed1b8c764f47afbbfbdb49ed6debdfd80dee
Nathan Herzing <nherzing@gmail.com>
2012-11-02 12:45:07 -0400

    Opening up voicemail url change url for now.

--- lib/crowdring/crowdring.rb
+++ lib/crowdring/crowdring.rb
@@ -64,7 +64,7 @@ module Crowdring
       end
     end
 
-    before /^((?!((voice|sms)response)|reports|login|resetpassword|progress-embed).)*$/ do
+    before /^((?!((voice|sms)response)|reports|login|resetpassword|voicemails|progress-embed).)*$/ do
       login_required unless settings.environment == :test
     end
 
