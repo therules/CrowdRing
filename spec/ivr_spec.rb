60d3ae467102e2206eca19ac850b4bc1adefb089
Willa <wwang@thoughtworks.com>
2013-01-18 11:26:09 -0500

    Refactoring ivr spec.

--- spec/ivr_spec.rb
+++ spec/ivr_spec.rb
@@ -11,10 +11,9 @@ describe Crowdring::Ivr do
   before(:each) do 
     DataMapper.auto_migrate!
     @params = {"question"=>"foo", "key_options"=>{"1"=>{"press"=>"1", "for"=>"bar"}}}
-    @campaign = Crowdring::Campaign.create(title: 'c3', voice_numbers: [{phone_number: '+18003333333', description: "c3 num"}], sms_number: '+18003333333')
     @ivr = Crowdring::Ivr.create(@params)
-    @campaign.ivrs << @ivr
-    @campaign.save
+    @stub_campaign = double('campaign', id: 1, ivrs: @ivr)
+
   end
   
   it '@campaign should be able to add a new ivr' do
@@ -22,28 +21,36 @@ describe Crowdring::Ivr do
   end
 
   it 'should create key option object when create a new ivr' do 
-    @ivr.key_options.count.should eq(1)
+    Crowdring::KeyOption.all.count.should eq(1)
   end
 
   it 'should set question' do 
     Crowdring::Ivr.last.question.should eq('foo')
   end
 
-  it 'should be able to update ringer count' do 
-    params = {Digits: '1', id: 1}
-    post '/ivrs/1/collect_digit', params
+  describe 'when having an activated ivr' do
+    before(:each) do
+      @campaign = Crowdring::Campaign.create(title: 'c3', voice_numbers: [{phone_number: '+18003333333', description: "c3 num"}], sms_number: '+18003333333')
+      @campaign.ivrs << @ivr
+      @campaign.save
+    end
 
-    Crowdring::KeyOption.first.ringer_count.should eq(1)
-  end
+    it 'should be able to update ringer count' do 
+      params = {Digits: '1', id: 1}
+      post "/ivrs/#{@campaign.id}/collect_digit", params
 
-  it 'should be able to disable ivr' do
-    post "/campaign/#{@campaign.id}/ivrs/disable"
+      Crowdring::KeyOption.first.ringer_count.should eq(1)
+    end
 
-    Crowdring::Ivr.first.activated.should be_false
-  end
+    it 'should be able to disable ivr' do
+      post "/campaign/#{@campaign.id}/ivrs/disable"
 
-  it 'should be able to destroy ivr' do
-    post "/campaign/#{@campaign.id}/ivrs/#{@ivr.id}/destroy"
-    Crowdring::Ivr.all.count.should eq(0)
+      Crowdring::Ivr.first.activated.should be_false
+    end
+
+    it 'should be able to destroy ivr' do
+      post "/campaign/#{@campaign.id}/ivrs/#{@ivr.id}/destroy"
+      Crowdring::Ivr.all.count.should eq(0)
+    end
   end
 end
