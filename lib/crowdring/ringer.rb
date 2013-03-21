e88ee2ef6fe3acd2e0a120c6fe0bf4748049c49d
Willa <wwang@thoughtworks.com>
2013-03-21 14:03:54 -0400

    Removed validation on phone numbers.

--- lib/crowdring/ringer.rb
+++ lib/crowdring/ringer.rb
@@ -23,7 +23,7 @@ module Crowdring
     has n, :rings, constraint: :destroy
     
     validates_presence_of :phone_number
-    validates_with_method :phone_number, :valid_phone_number?
+    # validates_with_method :phone_number, :valid_phone_number?
 
     after :create, :add_tags
 
